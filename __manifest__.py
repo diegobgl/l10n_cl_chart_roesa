24971f54e90ce5349cb050ce183128a8cf8550fd
diego gajardo <diegobgajardo@gmail.com>
2019-02-20 00:07:45 -0300

    up

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Chile Localization Chart Account',
+    'version': '1.0',
+    'description': """
+Chilean accounting chart and tax localization.
+==============================================
+    """,
+    'author': 'Carlos Lopez Mite',
+    'website': '',
+    'category': 'Localization',
+    'depends': ['account'],
+    'data': [
+        'data/account.account.tag.csv',
+        'data/l10n_cl_chart_data.xml',
+        'data/account.account.template.csv',
+        'data/account.tax.template.csv',
+        'data/l10n_cl_chart_data_after.xml',
+        'data/account_chart_template_data.yml',
+        #'views/account_view.xml',
+    ],
+    'post_init_hook': '_auto_install_l10n',
+}
